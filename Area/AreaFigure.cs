785640223c5d2b1c9222898af392d0a97c6c4bf0
eRKa <rlikh75@mail.ru>
2015-08-22 02:14:28 +0400

    Добавил округление при сравнении (поиск катетов)

--- Area/AreaFigure.cs
+++ Area/AreaFigure.cs
@@ -4,18 +4,22 @@ namespace Area
 {
     public static class AreaFigure
     {
+        // До какого знака округляем
+        const int delta = 1;
+
         public static decimal AreaRightTriangle(decimal a, decimal b, decimal c)
         {
             if (a < 0 || b < 0 || c < 0) throw new Exception("Отрицательные параметры недопустимы!");
             if (a + b < c || a + c < b || b + c < a) throw new Exception("Параметры не принадлежат треугольнику!");
 
-            if (a * a + b * b == c * c)
+            // Находим катеты. Вычичления при сравнении округляем до десятой
+            if (Math.Round(a * a + b * b, delta) == Math.Round(c * c, delta))
                 return 0.5m * a * b;
 
-            if (a * a + c * c == b * b)
+            if (Math.Round(a * a + c * c, delta) == Math.Round(b * b, delta))
                 return 0.5m * a * c;
 
-            if (b * b + c * c == a * a)
+            if (Math.Round(b * b + c * c, delta) == Math.Round(a * a, delta))
                 return 0.5m * b * c;
 
             throw new Exception("Параметры не прямоугольного треугольника!");
